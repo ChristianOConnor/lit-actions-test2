a2a94163fe1921c74c5d3f4aa5160363ee897239
ChristianOConnor <warpanomaly@gmail.com>
2023-02-07 06:05:41 -0500

    basic state function on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [resp, setResp] = useState('');
+  function callApi() {
+    setResp('this function works');
+    return;
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +15,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card">
+          <button onClick={() => callApi()}>click this to call API</button>
+          <p>{resp}</p>
+        </div>
       </header>
     </div>
   );
