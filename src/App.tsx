77344a4488435913f4ebed8bfcd3b9bcecf0218e
ChristianOConnor <warpanomaly@gmail.com>
2023-02-07 15:51:35 -0500

    add lit w signature not called and polyfill fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//@ts-ignore
+import LitJsSdk from 'lit-js-sdk/build/index.node.js';
 
 function App() {
   const [resp, setResp] = useState('');
-  function callApi() {
-    setResp('this function works');
-    return;
-  }
+
+  async function callApi() {
+    // you need an AuthSig to auth with the nodes
+    // this will get it from metamask or any browser wallet
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain: "mumbai" });
+  
+    const litNodeClient = new LitJsSdk.LitNodeClient({ litNetwork: "serrano" });
+    await litNodeClient.connect();
+  
+    const signatures = await litNodeClient.executeJs({
+      ipfsId: process.env.REACT_APP_IPFS_CID,
+      authSig,
+      // all jsParams can be used anywhere in your Lit Action Code
+      jsParams: {
+        // this is the string "Hello World" for testing
+        toSign: [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100],
+        publicKey:
+          process.env.REACT_APP_PUBLIC_KEY,
+        sigName: process.env.REACT_APP_SIG_NAME,
+      },
+    })
+  };
+
   return (
     <div className="App">
       <header className="App-header">
